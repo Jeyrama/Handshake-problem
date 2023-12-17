f83891fca971dfdd1d8a304b6f4fe509a6b1844b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-17 11:38:08 -0800

    Update Handshake-problem.js

--- Handshake-problem.js
+++ Handshake-problem.js
@@ -25,3 +25,8 @@ function getParticipants(handshakes) {
 }
 
 // or
+
+function getParticipants(handshakes) {
+  if (handshakes == 0) return 0
+  return Math.ceil((1+Math.sqrt(1+8*handshakes))/2)
+}

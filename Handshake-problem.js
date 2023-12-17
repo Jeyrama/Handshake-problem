c48a2007065d853242e6dc04bef9136f12b9ac22
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-17 11:33:46 -0800

    Update Handshake-problem.js

--- Handshake-problem.js
+++ Handshake-problem.js
@@ -12,3 +12,6 @@ Help Johnny by writing a function,
 that takes the amount of handshakes and returns the minimal amount of people 
 needed to perform these handshakes (a pair of farmers handshake only once).
 */
+
+
+// Solution
